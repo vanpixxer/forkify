3cee640dc3ce24e2d87f6013d3a3116a8e9d344f
Darrell <webtastic.dc@gmail.com>
2018-11-24 15:11:49 -0800

    Installed and configured Babel dependencies and devDependencies

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 // Global app controller
 import num from './test';
+const x = 23;
 
-console.log(`I imported ${num} from another module called test.js`);
+console.log(`I imported ${num} from another module called test.js. Variable x is ${x}`);
